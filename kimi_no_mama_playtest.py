6ee69d5992787fc59c67ea1da4ebf2d0c4d2f90f
bluaxees <45705751+bluaxees@users.noreply.github.com>
2018-12-09 02:46:47 +0800

    added dictionaries, rough draft for class activeScene

--- kimi_no_mama_playtest.py
+++ kimi_no_mama_playtest.py
@@ -12,11 +12,15 @@ pygame.display.set_caption('Kimi no Mama')
 gameIcon = pygame.image.load('assets/icon.png')
 pygame.display.set_icon(gameIcon)
 
-#a dictionary of scenes and their background
+#a dictionary of everything
 scenery = {'Scene1': 'dark_background', 'Scene2': 'placeholder_bg1', 'Scene3': 'placeholder_green', 'Scene4': 'placeholder_red', 'Scene5': 'placeholder_blue', 'Scene6': 'placeholder_purple'}
 character = {'Scene1': 'mom-angry', 'Scene2': 'mom-happy', 'Scene3': 'mom-oface', 'Scene4': 'mom-solo', 'Scene5': 'mom-soloangry', 'Scene6': 'mom-solofinger'}
-dialogue = {'Scene1': [('Nayeon', 'you and me in the moonlight'), ('Nayeon', 'byeol kkot chukje yeollin bam'), ('Sana','pado sorireul teulgo'), ('Sana', 'chumeul chuneon ni sungan'), ('Sana', 'i neukkim jeongmal ttagya')], 'Scene2': [('Momo', 'badaya uriwa gachi nora'), ('Momo', 'barama neodo ijjogeuro wa')], 'Scene3': [('Jeongyeon', 'dalppit jomyeong araeseo'), ('Jeongyeon', 'neowa nawa sesanggwa'), ('Jeongyeon', 'da gachi Party all night long'), ('Jeongyeon', 'Yeah, it’s good')], 'Scene4': [('Tzuyu', 'If you wanna have some fun'), ('Tzuyu', 'jjapjjalhan gonggicheoreom'), ('Tzuyu', 'i sungane teukbyeolhan'), ('Tzuyu',  'haengbogeul notchiji ma')], 'Scene5': [('Mina', 'One, two, three, let\'s go'), ('Mina', 'chouju wiro'), ('Chaeyoung', 'narageul deut chumchureoga hey'), ('Chaeyoung', 'oh let\'s dance the night away')], 'Scene6': [('Chaeyoung', 'Let’s dance the night away'), ('Jihyo', 'One, two, three, let’s go'), ('Jihyo', 'jeo bada geonneo'), ('Jihyo', 'deullil deut sori jilleo'), ('Jihyo', 'Let’s dance the night away')]}
+scene_types = {'Scene1': 'passiveScene', 'Scene2': 'passiveScene', 'Scene3': 'passiveScene', 'Scene4': 'passiveScene', 'Scene5': 'passiveScene', 'Scene6': 'passiveScene'}
+dialogue = {'Scene1': [('', '*awesome intro*'), ('Nayeon', 'you and me in the moonlight'), ('Nayeon', 'byeol kkot chukje yeollin bam'), ('Sana','pado sorireul teulgo'), ('Sana', 'chumeul chuneon ni sungan'), ('Sana', 'i neukkim jeongmal ttagya')], 'Scene2': [('Momo', 'badaya uriwa gachi nora'), ('Momo', 'barama neodo ijjogeuro wa')], 'Scene3': [('Jeongyeon', 'dalppit jomyeong araeseo'), ('Jeongyeon', 'neowa nawa sesanggwa'), ('Jeongyeon', 'da gachi Party all night long'), ('Jeongyeon', 'Yeah, it’s good')], 'Scene4': [('Tzuyu', 'If you wanna have some fun'), ('Tzuyu', 'jjapjjalhan gonggicheoreom'), ('Tzuyu', 'i sungane teukbyeolhan'), ('Tzuyu',  'haengbogeul notchiji ma')], 'Scene5': [('Mina', 'One, two, three, let\'s go'), ('Mina', 'chouju wiro'), ('Chaeyoung', 'narageul deut chumchureoga hey'), ('Chaeyoung', 'let\'s dance the night away')], 'Scene6': [('', '*music intensifies*'), ('Chaeyoung', 'Let’s dance the night away'), ('Jihyo', 'One, two, three, let’s go'), ('Jihyo', 'jeo bada geonneo'), ('Jihyo', 'deullil deut sori jilleo'), ('Jihyo', 'Let’s dance the night away')]}
 nexts = {'Scene1': 'Scene2', 'Scene2': 'Scene3', 'Scene3': 'Scene4', 'Scene4': 'Scene5', 'Scene5': 'Scene6', 'Scene6': 'Scene1'}
+choices = {'dScene1': ('Yes', 'or', 'Yes'), 'dScene2': ('Yes', 'or', 'Yes'), 'dScene3': ('Yes', 'or', 'Yes'), 'dScene4': ('Yes', 'or', 'Yes'), 'dScene5': ('Yes', 'or', 'Yes'), 'dScene6': ('Yes', 'or', 'Yes')}
+outcome_text = {'dScene1': (('What', 'is', 'Love?'), ('jjirit', 'jjirit', 'jjirit', 'jjirit'), ('You\'re', 'my', 'heartshaker shaker')), 'dScene2': (('What', 'is', 'Love?'), ('jjirit', 'jjirit', 'jjirit', 'jjirit'), ('You\'re', 'my', 'heartshaker shaker')), 'dScene3': (('What', 'is', 'Love?'), ('jjirit', 'jjirit', 'jjirit', 'jjirit'), ('You\'re', 'my', 'heartshaker shaker')), 'dScene4': (('What', 'is', 'Love?'), ('jjirit', 'jjirit', 'jjirit', 'jjirit'), ('You\'re', 'my', 'heartshaker shaker'))}
+outcome_action = outcome_text = {'dScene1': ('dScene4', 'dScene2', 'dScene3'), 'dScene2': ('dScene1', 'dScene4', 'dScene3'), 'dScene3': ('dScene1', 'dScene2', 'dScene4'), 'dScene4': ('dScene3', 'dScene2', 'dScene1')}
 
 #colors in a tuple
 black = (0, 0, 0)
@@ -84,7 +88,7 @@ class renderImage: #filename, image_type = '', pos_x = 0, pos_y = 0, path = 'ima
 
 class displayText: #parameters: text_list, line = 0, size = 45, color = white, pos_x = 0, pos_y = 0, font = None
 
-	def __init__(self, text_list, line = 0, size = 45, color = white, pos_x = 0, pos_y = 0, font = 'assets/DeJavuSans.ttf'):
+	def __init__(self, text_list, line = 0, size = 35, color = white, pos_x = 0, pos_y = 0, font = 'assets/DeJavuSans.ttf'):
 		global display_height
 		global panel_offset
 		global panel_height
@@ -100,7 +104,7 @@ class displayText: #parameters: text_list, line = 0, size = 45, color = white, p
 	def passive_panel(self):
 
 		fontography = pygame.font.Font(self.font, self.size)
-		textSurface = fontography.render(self.text_list[self.line], True, self.color)
+		textSurface = fontography.render(self.text_list, True, self.color)
 		textRect = textSurface.get_rect()
 		textRect.midleft = (self.pos_x, self.pos_y)
 		gameDisplay.blit(textSurface, textRect)
@@ -111,6 +115,7 @@ class displayText: #parameters: text_list, line = 0, size = 45, color = white, p
 		string = ''
 		fontography = pygame.font.Font(self.font, self.size)
 		for character in self.text_list:
+			pygame.time.wait(40)
 			string += character
 			textSurface = fontography.render(string, True, self.color)
 			textRect = textSurface.get_rect()
@@ -118,7 +123,7 @@ class displayText: #parameters: text_list, line = 0, size = 45, color = white, p
 			gameDisplay.blit(textSurface, textRect)
 			pygame.display.update()
 			clock.tick(60)
-			pygame.time.wait(40)
+
 
 	def passive(self):
 
@@ -129,7 +134,7 @@ class displayText: #parameters: text_list, line = 0, size = 45, color = white, p
 		gameDisplay.blit(textSurface, textRect)
 		pygame.display.update()
 
-class passiveScene: #scene_name, next_scene = None, next_type = 'passiveScene'
+class passiveScene: #scene_name, next_type = 'passiveScene'
 
 	def __init__(self, scene_name, next_type = 'passiveScene'):
 
@@ -142,9 +147,8 @@ class passiveScene: #scene_name, next_scene = None, next_type = 'passiveScene'
 		self.scene_done = False
 		self.line = 0
 		self.scene_name = scene_name
-		#self.speaker_name, self.text_list = dialogue.get(self.scene_name)[self.line]
 		self.next_scene = nexts.get(self.scene_name)
-		self.next_type = next_type
+		self.next_type = string_to_callable(next_type)
 		self.speaker_pos_x = 135
 		self.speaker_pos_y = 518
 		self.text_pos_x = 200
@@ -172,7 +176,7 @@ class passiveScene: #scene_name, next_scene = None, next_type = 'passiveScene'
 						renderImage('panel', '', 0, self.panel_pos_y).midtop()
 						self.line += 1
 						if self.line == len(dialogue.get(self.scene_name)):
-							string_to_callable(self.next_type)(self.next_scene).execute()
+							self.next_type(self.next_scene).execute()
 						self.scene_done = False
 			self.speaker_name, self.text_list = dialogue.get(self.scene_name)[self.line]
 			if not self.scene_done:
@@ -181,16 +185,28 @@ class passiveScene: #scene_name, next_scene = None, next_type = 'passiveScene'
 				self.scene_done = True
 				continue
 			else:
-				#displayText(self.text_list, self.line, self.font_size, self.color, 150, 400).passive_panel()
+				renderImage(scenery.get(self.scene_name), 'scenery/').center()
+				renderImage(character.get(self.scene_name), 'character/', 0, self.char_pos_y).midtop()
+				renderImage('panel', '', 0, self.panel_pos_y).midtop()
+				displayText(self.speaker_name, 0, self.speaker_text_size, self.color, self.speaker_pos_x, self.speaker_pos_y).passive()
+				displayText(self.text_list, self.line, self.dialog_text_size, self.color, self.text_pos_x, 0).passive_panel()
+
 				self.scene_done = True
 				continue
 
+			clock.tick(60)
+
 class activeScene:
 
-	def __init__(self):
-		pass
+	def __init__(self, scene_name, next_scene_list, next_type = 'passiveScene'):
+		global scenery
+		global choices
+		global outcome_action
+		global outcome_text
+		self.game_quit = False
 
-passiveScene('Scene1').execute()
+	def execute(self):
+		pass
 
 def start():
 
@@ -213,6 +229,8 @@ def start():
 		pygame.display.update()
 
 		clock.tick(60)
+
+passiveScene('Scene1').execute()
 
 def screen1():
 
@@ -282,121 +300,3 @@ def screen4():
 start()
 pygame.quit()
 quit()
-
-'''
-display_text_passive(text_list[line], size, color, pos_x, pos_y)
-	
-	if gameDisplay.get_rect().collidepoint(mouse_position):
-		if left_click:
-			line += 1
-			display_text_active(text_list, line, size, color, pos_x, pos_y)
-	else:
-		pass
-
-
-class Button:
-
-	def __init__(self, filename, pos_x, pos_y, action = None, text = None, extension = '.png', path = 'images/', button_width = 175, button_height = 50):
-		global transparent
-		self.mouse_position = pygame.mouse.get_pos()
-		self.left_click = pygame.mouse.get_pressed()[0]
-		self.pos_x = pos_x
-		self.pos_y = pos_y
-		self.button_width = button_width
-		self.button_height = button_height
-		self.buttonRect = pygame.draw.rect(gameDisplay, transparent, [self.pos_x, self.pos_y, self.button_width, self.button_height])
-		self.imagename_passive = path + filename + extension
-		self.imagename_active = path + 'hover' + filename + extension
-
-	def action(self):
-		if self.buttonRect.collidepoint(self.mouse_position):
-			renderImage()
-		else:
-			renderImage()
-
-largeText = pygame.font.Font('freesansbold.ttf', 115)
-TextSurf, TextRect = text_objects('EYYY', largeText)
-TextRect.center = ((width/2), height/2)
-gameDisplay.blit(TextSurf, TextRect)
-
-#pygame.draw.rect(gameDisplay, green, (150, 450, 100, 50))
-#pygame.draw.rect(gameDisplay, red, (550, 450, 100, 50))
-#gameDisplay.blit(image, (550, 450, 100, 50))
-
-mouse = pygame.mouse.get_pos()
-#print(mouse)
-
-def text_objects(text, font):
-	textSurface = font.render(text, True, black)
-	return textSurface, textSurface.get_rect()
-
-def message_display(text):
-	largeText = pygame.font.Font('freesansbold.ttf', 115)
-	TextSurf, TextRect = text_objects(text, largeText)
-	TextRect.center = ((width/2),(height/2))
-	gameDisplay.blit(TextSurf, TextRect)
-
-	pygame.display.update()
-	time.sleep(2)
-	#start()
-
-
-see_through = pygame.Surface((100,100)).convert_alpha()
-see_through.fill(BLU_HIGHLIGHT)
-see_through_rect = see_through.get_rect(topleft = screen_rect.center)
-
-import pygame
-
-pygame.init()
-
-gameDisplay = pygame.display.set_mode((1200,750), pygame.FULLSCREEN)
-
-pygame.font.init()
-myfont = pygame.font.SysFont('Comic Sans MS', 30)
-
-textSurface = myfont.render('Some Text', False, (255,255,255))
-gameDisplay.blit(textSurface, (20,30))
-
-pygame.display.update()
-
-	
-	#smallText = pygame.font.SysFont("comicsansms",20)
-	#textSurf, textRect = text_objects(text, smallText)
-	#if centered use this, otherwise use pos_x + some small value
-
-	#textRect.center = ((pos_x+(button_width/2)), (pos_y+(button_height/2))) 
-	#gameDisplay.blit(textSurf, textRect)
-
-def screen2():
-
-	gameDisplay.fill(white)
-	renderImage('placeholder_red').center()
-	game_done = False
-	scene_done = False
-
-	while not game_done:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				quit()
-
-		renderImage('mom').center()
-
-		if not scene_done:
-			print(scene_done)
-			displayText(['One, two, three, let\'s go', 'chouju wiro', 'narageul deut chumchureoga hey', 'oh let\'s dance the night away'], 0, 45, white, 100, 530).active()
-			scene_done = True
-		else:
-			print(scene_done)
-			displayText(['One, two, three, let\'s go', 'chouju wiro', 'narageul deut chumchureoga hey', 'oh let\'s dance the night away'], 0, 45, white, 100, 530).passive()
-			scene_done = True
-
-		button(100, 450, 'button1', screen1, 175, 50)
-		button(500, 450, 'button2', screen2, 175, 50)
-		button(100, 510, 'button3', screen3, 175, 50)
-		button(500, 510, 'button4', screen4, 175, 50)
-
-		pygame.display.update()
-		clock.tick(60)
-
-'''
--- my_trial_on_classses.py
+++ my_trial_on_classses.py
@@ -60,3 +60,121 @@ while True:
  
     clock.tick(20)
 
+
+'''
+display_text_passive(text_list[line], size, color, pos_x, pos_y)
+    
+    if gameDisplay.get_rect().collidepoint(mouse_position):
+        if left_click:
+            line += 1
+            display_text_active(text_list, line, size, color, pos_x, pos_y)
+    else:
+        pass
+
+
+class Button:
+
+    def __init__(self, filename, pos_x, pos_y, action = None, text = None, extension = '.png', path = 'images/', button_width = 175, button_height = 50):
+        global transparent
+        self.mouse_position = pygame.mouse.get_pos()
+        self.left_click = pygame.mouse.get_pressed()[0]
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.button_width = button_width
+        self.button_height = button_height
+        self.buttonRect = pygame.draw.rect(gameDisplay, transparent, [self.pos_x, self.pos_y, self.button_width, self.button_height])
+        self.imagename_passive = path + filename + extension
+        self.imagename_active = path + 'hover' + filename + extension
+
+    def action(self):
+        if self.buttonRect.collidepoint(self.mouse_position):
+            renderImage()
+        else:
+            renderImage()
+
+largeText = pygame.font.Font('freesansbold.ttf', 115)
+TextSurf, TextRect = text_objects('EYYY', largeText)
+TextRect.center = ((width/2), height/2)
+gameDisplay.blit(TextSurf, TextRect)
+
+#pygame.draw.rect(gameDisplay, green, (150, 450, 100, 50))
+#pygame.draw.rect(gameDisplay, red, (550, 450, 100, 50))
+#gameDisplay.blit(image, (550, 450, 100, 50))
+
+mouse = pygame.mouse.get_pos()
+#print(mouse)
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf', 115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((width/2),(height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+    time.sleep(2)
+    #start()
+
+
+see_through = pygame.Surface((100,100)).convert_alpha()
+see_through.fill(BLU_HIGHLIGHT)
+see_through_rect = see_through.get_rect(topleft = screen_rect.center)
+
+import pygame
+
+pygame.init()
+
+gameDisplay = pygame.display.set_mode((1200,750), pygame.FULLSCREEN)
+
+pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+
+textSurface = myfont.render('Some Text', False, (255,255,255))
+gameDisplay.blit(textSurface, (20,30))
+
+pygame.display.update()
+
+    
+    #smallText = pygame.font.SysFont("comicsansms",20)
+    #textSurf, textRect = text_objects(text, smallText)
+    #if centered use this, otherwise use pos_x + some small value
+
+    #textRect.center = ((pos_x+(button_width/2)), (pos_y+(button_height/2))) 
+    #gameDisplay.blit(textSurf, textRect)
+
+def screen2():
+
+    gameDisplay.fill(white)
+    renderImage('placeholder_red').center()
+    game_done = False
+    scene_done = False
+
+    while not game_done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        renderImage('mom').center()
+
+        if not scene_done:
+            print(scene_done)
+            displayText(['One, two, three, let\'s go', 'chouju wiro', 'narageul deut chumchureoga hey', 'oh let\'s dance the night away'], 0, 45, white, 100, 530).active()
+            scene_done = True
+        else:
+            print(scene_done)
+            displayText(['One, two, three, let\'s go', 'chouju wiro', 'narageul deut chumchureoga hey', 'oh let\'s dance the night away'], 0, 45, white, 100, 530).passive()
+            scene_done = True
+
+        button(100, 450, 'button1', screen1, 175, 50)
+        button(500, 450, 'button2', screen2, 175, 50)
+        button(100, 510, 'button3', screen3, 175, 50)
+        button(500, 510, 'button4', screen4, 175, 50)
+
+        pygame.display.update()
+        clock.tick(60)
+
+'''
